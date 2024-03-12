6250177e1f3a7ccfcc558286b86910d38fd1c4c4
Petik Péter <petik2peter@gmail.com>
2024-03-12 12:07:37 +0100

    clean

--- publishconf.py
+++ publishconf.py
@@ -15,8 +15,8 @@ from pelicanconf import *
 SITEURL = 'https://p371k9.github.io/northwind'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+#FEED_ALL_ATOM = 'feeds/all.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
